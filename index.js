09c6157ab65c784d64fa20c763d44c0fd00fb8be
Carol da Rosa <caroolpach@outlook.com>
2019-07-11 23:26:53 -0300

    1.2.8

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+var requestTime = (req, res, next) => {
+  req.requestTime = new Date();
+  next()
+}
+
+app.use(requestTime)
+
 app.get('/', (req, res) => {
-    var requestTime =  new Date().toLocaleTimeString();
-    var responseText = `<h1>Hello World!</h1>`;
-    responseText += `AGORA SÃO: ` + requestTime + '';
-    res.send(responseText);
-  });
+  var hora = req.requestTime.getHours();
+  var min = req.requestTime.getMinutes();
+  var responseText = `<h1>Hello World!</h1>`;
+  responseText += `AGORA SÃO: ` + hora + ':' + min + '';
+  res.send(responseText);
+});
 
 app.listen(port, () => console.log(`Mano Example app listening on port ${port}!`));
